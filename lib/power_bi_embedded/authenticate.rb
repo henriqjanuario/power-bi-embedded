1be4f5783fd17032b960034efdd803011a736560
Dayves R Dias <dayves@jera.com.br>
2020-08-14 10:47:34 -0400

    I tryied to use but the auth need a sclient_secret so a added it to work fine

--- lib/power_bi_embedded/authenticate.rb
+++ lib/power_bi_embedded/authenticate.rb
@@ -4,11 +4,12 @@ module PowerBiEmbedded
     BASE_URL   = "https://login.windows.net/common/oauth2/token"
     RESOURCE   = "https://analysis.windows.net/powerbi/api"
 
-    def initialize(username: nil, password: nil, refresh_token: nil, client_id: nil, grant_type: nil)
+    def initialize(username: nil, password: nil, refresh_token: nil, client_id: nil, client_secret: nil, grant_type: nil)
       @username      = username
       @password      = password
       @refresh_token = refresh_token
       @client_id     = client_id
+      @client_secret = client_secret
       @grant_type    = grant_type
     end
 
@@ -21,6 +22,7 @@ module PowerBiEmbedded
     def build_body
       {
         client_id: @client_id,
+        client_secret: @client_secret,
         grant_type: @grant_type,
         resource: RESOURCE,
         username: @username,
